672e38050c1b09066ca27349a7542bd6168211af
Rashmi Dulashani <dulashanirsh@gmail.com>
2026-01-18 06:12:03 -0800

    Add CI Pipeline using GitHub Actions

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 mt-20">
+      <div className="border-t border-gray-700 text-center py-8 text-sm">
+        © 2026 Q-Master. All rights reserved.
+      </div>
+    </footer>
+  );
+}
